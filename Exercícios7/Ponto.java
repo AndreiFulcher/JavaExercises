7d8da0cc5e1408fb095160b5bef375b56a900a09
AndreiFulcher <andreii.fulcher@gmail.com>
2025-09-13 18:40:50 -0300

    Exercício Ponto de Coordenadas (Sobrecarga de método)

--- Exercícios7/Ponto.java
+++ Exercícios7/Ponto.java
@@ -1,4 +1,47 @@
 package Exercícios7;
 
-public class TestaRelogio {
+public class Ponto {
+
+    private double x;
+    private double y;
+
+    public Ponto(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Ponto() {
+        this(1.0, 1.0);
+    }
+
+    public Ponto(Ponto p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public void setX(double xVal) {
+        x = xVal;
+    }
+
+    public void setY(double yVal) {
+        y = yVal;
+    }
+
+    public void desloca(double dx, double dy) {
+        x = x + dx;
+        y = y + dy;
+    }
+
+    public double getX() {
+        return (x);
+    }
+
+    public double getY() {
+        return (y);
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
 }
+
--- Exercícios7/TestaPonto.java
+++ Exercícios7/TestaPonto.java
@@ -1,4 +1,15 @@
 package Exercícios7;
 
 public class TestaPonto {
+    public static void main(String[] args) {
+        Ponto p1 = new Ponto();
+        Ponto p2 = new Ponto(2.0, 3.0);
+        Ponto p3 = new Ponto(p2);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        p3.desloca(3.0, 0.0);
+        System.out.println(p3);
+    }
+
 }
